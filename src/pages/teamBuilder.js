3d18d9a5f833ba5d10b96dea3024cb651413b7fb
AnOwlbear <AnOwlbear@protonmail.com>
2021-03-28 20:46:05 +0100

    Added missing key to TeamPlaceholders

--- src/pages/teamBuilder.js
+++ src/pages/teamBuilder.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import TeamPokemon from '../components/teamPokemon';
 import TeamPlaceholder from '../components/teamPlaceholder';
 
 const TeamBuilder = ({ className }) => {
-  const dispatch = useDispatch();
   const team = useSelector(x => x.team);
 
   return (
     <div className={className}>
       <h1 className="text-3xl font-bold text-center">Team builder</h1>
       {team.map(x => <TeamPokemon key={x} name={x} />)}
-      {[...Array(6 - team.length).keys()].map(_ => <TeamPlaceholder />)}
+      {[...Array(6 - team.length).keys()].map(x => <TeamPlaceholder key={`placeholder-${x}`} />)}
     </div>
   )
 };
