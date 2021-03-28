e31ea45ae898f400e7072fe79a8697549cf45dfc
AnOwlbear <AnOwlbear@protonmail.com>
2021-03-28 20:55:22 +0100

    Added checks to prevent too many or multiple of the same pokemon being added to a team

--- src/pages/pokemon.js
+++ src/pages/pokemon.js
@@ -5,12 +5,14 @@ import { getPokemonInfo } from '../services/pokemonStoreService';
 import TwoPartText from '../components/twoPartText';
 import Stat from '../components/stat';
 import MoveLink from '../components/moveLink';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addTeamPokemon } from '../reducers/teamReducer';
 import { getEggMoves, getLevelUpMoves } from '../tools';
+import { addError } from '../reducers/errorReducer';
 
 const Pokemon = () => {
   const dispatch = useDispatch();
+  const teamPokemon = useSelector(x => x.team);
   let name = useParams().id;
 
   const [pokemon, setPokemon] = useState({});
@@ -31,6 +33,21 @@ const Pokemon = () => {
     loadPokemon();
   }, [name]);
 
+  // Adds a pokemon to the team, displaying an error if it is already full or already has the pokemon
+  const handleAddTeamPokemon = () => {
+    console.log(teamPokemon);
+    if (teamPokemon.length >= 6) {
+      dispatch(addError('Team already has 6 pokemon'));
+      return;
+    }
+    if (teamPokemon.some(x => x === name)) {
+      dispatch(addError(`${name} is already on your team`));
+      return;
+    }
+
+    dispatch(addTeamPokemon(name));
+  };
+
   return (
     <>
       {loading && <Spinner className="w-14 h-14 mx-auto mt-4" />}
@@ -41,7 +58,7 @@ const Pokemon = () => {
             <TwoPartText className="text-3xl font-bold" textOne={pokemon.info?.name} textTwo={`#${pokemon.info?.id}`} />
             <p className="my-4 text-2xl font-semibold">Base Stats</p>
             {pokemon.info?.stats.map(x => <Stat key={x.stat.name} name={x.stat.name} amount={x.base_stat} />)}
-            <button className="block w-full my-4 p-4 text-white bg-red-500 rounded shadow-lg" onClick={() => dispatch(addTeamPokemon(name))}>Add to team</button>
+            <button className="block w-full my-4 p-4 text-white bg-red-500 rounded shadow-lg" onClick={handleAddTeamPokemon}>Add to team</button>
           </div>
           <div className="flex-grow">
             <p className="text-3xl font-bold mb-4">Level up moves</p>
